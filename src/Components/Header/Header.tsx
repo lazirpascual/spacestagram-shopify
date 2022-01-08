1dc2ab718086b861d7d1bf964b8eedb49ca2e98c
Lazir Pascual <lazirpascual@gmail.com>
2022-01-08 00:45:52 -0500

    add nasa logo and icon

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import Divider from "@mui/material/Divider";
+import "./Header.css";
 
 const Header: React.FC = () => {
   return (
     <div>
-      <Typography variant="h3" gutterBottom sx={{ marginTop: 3 }}>
-        Spacestagram
-      </Typography>
+      <div className="logo-container">
+        <img
+          src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
+          height="50px"
+          width="100px"
+          alt="Nasa Logo"
+        />
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{ marginTop: 3, marginRight: 5 }}
+        >
+          Spacestagram
+        </Typography>
+      </div>
       <Typography variant="subtitle1" gutterBottom>
-        Brought to you by NASA’s APOD image API.
+        Brought to you by NASA’s Astronomy Picture of the Day (APOD) API.
       </Typography>
       <Divider sx={{ marginBottom: 6 }} />
     </div>
