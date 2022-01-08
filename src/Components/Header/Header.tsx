6c9f74bfce17e87650be57856d8d19ca2d4025c6
Lazir Pascual <lazirpascual@gmail.com>
2022-01-08 01:09:46 -0500

    make header component stick to the top on scroll

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,26 +5,26 @@ import "./Header.css";
 
 const Header: React.FC = () => {
   return (
-    <div>
+    <div className="header">
       <div className="logo-container">
         <img
           src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"
-          height="50px"
-          width="100px"
+          height="40px"
+          width="80px"
           alt="Nasa Logo"
         />
         <Typography
           variant="h3"
           gutterBottom
-          sx={{ marginTop: 3, marginRight: 5 }}
+          sx={{ marginTop: 2, marginRight: 4 }}
         >
           Spacestagram
         </Typography>
       </div>
       <Typography variant="subtitle1" gutterBottom>
-        Brought to you by NASA’s Astronomy Picture of the Day (APOD) API.
+        Presented by NASA’s Astronomy Picture of the Day (APOD) API
       </Typography>
-      <Divider sx={{ marginBottom: 6 }} />
+      <Divider />
     </div>
   );
 };
