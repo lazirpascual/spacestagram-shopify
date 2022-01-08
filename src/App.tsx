ce05a72a6e8038a7911aa551a44bc960e5fbef12
Lazir Pascual <lazirpascual@gmail.com>
2022-01-07 22:26:00 -0500

    add loading state while waiting to fetch for API

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { fetchImagesData } from "./Api";
-import "./App.css";
 import ImageSection from "./Components/ImageSection/ImageSection";
 import Header from "./Components/Header/Header";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import { CurrentImage } from "./Interfaces/Interface";
+import "./App.css";
 
 function App() {
   const [imagesList, setImagesList] = useState<CurrentImage[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+  const [isFetchingButton, setIsFetchingButton] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const imagesData = await fetchImagesData(9);
+      if (imagesData) {
+        setIsFetching(false);
+      }
       setImagesList(imagesData);
     };
 
@@ -19,21 +25,37 @@ function App() {
   }, []);
 
   const handleLoadClick = async () => {
+    setIsFetchingButton(true);
     const imagesData = await fetchImagesData(6);
+    if (imagesData) {
+      setIsFetchingButton(false);
+    }
     setImagesList([...imagesList, ...imagesData]);
   };
 
   return (
     <div className="App">
       <Header />
-      <ImageSection imagesList={imagesList} />
-      <Button
-        onClick={handleLoadClick}
-        variant="outlined"
-        sx={{ marginTop: 7, marginBottom: 9 }}
-      >
-        Load More
-      </Button>
+      {isFetching ? (
+        <div>
+          <CircularProgress /> <br />
+        </div>
+      ) : (
+        <ImageSection imagesList={imagesList} />
+      )}
+      {isFetchingButton ? (
+        <div>
+          <CircularProgress sx={{ marginTop: 7, marginBottom: 9 }} /> <br />
+        </div>
+      ) : (
+        <Button
+          onClick={handleLoadClick}
+          variant="outlined"
+          sx={{ marginTop: 7, marginBottom: 9 }}
+        >
+          Load More
+        </Button>
+      )}
     </div>
   );
 }
